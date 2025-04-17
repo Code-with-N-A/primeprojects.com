982a1bd851580eb0bd23bcead5970b7c95513185
codewith_N.A <codewithna73@gmail.com>
2025-04-17 18:41:12 +0530

    all file update

--- sin.js
+++ sin.js
@@ -1,4 +1,4 @@
-
-function sinUp(){
-    window.location.href='ragistretion.html';
-    };
+const user = JSON.parse(localStorage.getItem('userData'));
+    if (!user || !user.verified) {
+      window.location.href = "ragistretion.html";
+    }
